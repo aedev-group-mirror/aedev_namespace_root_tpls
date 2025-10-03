d56227552a441c165aea6deda1e5a99512a920c3
AndiEcker <aecker2@gmail.com>
2025-10-03 12:12:55 +0100

    v0.3.20: updated to use pjm instead of grm in CI integrity check
    M  .gitignore
    M  .gitlab-ci.yml
    M  CONTRIBUTING.rst
    M  LICENSE.md
    M  README.md
    M  SECURITY.md
    M  aedev/namespace_root_tpls/__init__.py
    M  dev_requirements.txt
    M  setup.py
    M  tests/conftest.py
    M  tests/requirements.txt
    M  tests/test_namespace_root_tpls.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.52
+# THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.project_tpls v0.3.58
 """ setup of aedev namespace package portion namespace_root_tpls: templates and outsourced files for namespace root projects.. """
 # noinspection PyUnresolvedReferences
 import sys
@@ -15,25 +15,25 @@ setup_kwargs = {
         'Programming Language :: Python :: 3.9', 'Topic :: Software Development :: Libraries :: Python Modules',
         'Typing :: Typed'],
     'description': 'aedev namespace package portion namespace_root_tpls: templates and outsourced files for namespace root projects.',
-    'extras_require': {       'dev': [       'aedev_project_tpls', 'aedev_aedev', 'anybadge', 'coverage-badge', 'aedev_git_repo_manager',
+    'extras_require': {       'dev': [       'aedev_project_tpls', 'aedev_aedev', 'anybadge', 'coverage-badge', 'aedev_project_manager',
                        'flake8', 'mypy', 'pylint', 'pytest', 'pytest-cov', 'pytest-django', 'typing',
                        'types-setuptools'],
         'docs': [],
-        'tests': [       'anybadge', 'coverage-badge', 'aedev_git_repo_manager', 'flake8', 'mypy', 'pylint', 'pytest',
+        'tests': [       'anybadge', 'coverage-badge', 'aedev_project_manager', 'flake8', 'mypy', 'pylint', 'pytest',
                          'pytest-cov', 'pytest-django', 'typing', 'types-setuptools']},
     'install_requires': [],
     'keywords': ['configuration', 'development', 'environment', 'productivity'],
     'license': 'GPL-3.0-or-later',
     'long_description': ('<!-- THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.aedev v0.3.26 -->\n'
  '<!-- THIS FILE IS EXCLUSIVELY MAINTAINED by the project aedev.namespace_root_tpls v0.3.18 -->\n'
- '# namespace_root_tpls 0.3.19\n'
+ '# namespace_root_tpls 0.3.20\n'
  '\n'
  '[![GitLab '
  'develop](https://img.shields.io/gitlab/pipeline/aedev-group/aedev_namespace_root_tpls/develop?logo=python)](\n'
  '    https://gitlab.com/aedev-group/aedev_namespace_root_tpls)\n'
  '[![LatestPyPIrelease](\n'
- '    https://img.shields.io/gitlab/pipeline/aedev-group/aedev_namespace_root_tpls/release0.3.19?logo=python)](\n'
- '    https://gitlab.com/aedev-group/aedev_namespace_root_tpls/-/tree/release0.3.19)\n'
+ '    https://img.shields.io/gitlab/pipeline/aedev-group/aedev_namespace_root_tpls/release0.3.20?logo=python)](\n'
+ '    https://gitlab.com/aedev-group/aedev_namespace_root_tpls/-/tree/release0.3.20)\n'
  '[![PyPIVersions](https://img.shields.io/pypi/v/aedev_namespace_root_tpls)](\n'
  '    https://pypi.org/project/aedev-namespace-root-tpls/#history)\n'
  '\n'
@@ -114,7 +114,7 @@ setup_kwargs = {
         'Source': 'https://aedev.readthedocs.io/en/latest/_modules/aedev/namespace_root_tpls.html'},
     'python_requires': '>=3.9',
     'url': 'https://gitlab.com/aedev-group/aedev_namespace_root_tpls',
-    'version': '0.3.19',
+    'version': '0.3.20',
     'zip_safe': False,
 }
 
