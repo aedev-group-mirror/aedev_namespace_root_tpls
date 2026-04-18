290495957a4dffa0244c298d984167a52ce795ba
AndiEcker <aecker2@gmail.com>
2026-04-18 13:34:29 +0100

    v0.3.29: remove duplicate wording in installation section of README-template
    M  README.md
    M  aedev/namespace_root_tpls/__init__.py
    M  aedev/namespace_root_tpls/templates/de_mtp_templates/de_otf__z_de_spt_namespace-root_de_otf_de_tpl_README.md
    M  setup.py

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup_kwargs = {
     },
     'python_requires': '>=3.12',
     'url': 'https://gitlab.com/aedev-group/aedev_namespace_root_tpls',
-    'version': '0.3.28',
+    'version': '0.3.29',
     'zip_safe': False,
 }
 
